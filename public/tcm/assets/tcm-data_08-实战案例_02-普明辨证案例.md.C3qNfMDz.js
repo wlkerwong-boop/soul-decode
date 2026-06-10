f16b8f0feb716a850bb2fcb8aef9501b040badcf
光明喜舍 <guangmingxishe@Mac.lan>
2026-06-10 20:27:44 +0800

    fix: remove private health data from public TCM site

--- public/tcm/assets/tcm-data_08-实战案例_02-普明辨证案例.md.C3qNfMDz.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/framework.Pywvta2k.js";import{__pageData as o,default as p}from"./08-实战案例_02-普明辨证案例.md.yODDv3Yd.js";export{o as __pageData,p as default};
--- public/tcm/assets/tcm-data_11-八字命理_01-普光普明八字深层分析.md.BJ_WmGgH.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/framework.Pywvta2k.js";import{__pageData as o,default as p}from"./11-八字命理_01-普光普明八字深层分析.md.GEZTOBYz.js";export{o as __pageData,p as default};
